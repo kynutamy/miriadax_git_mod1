1a88fea25f9b7a3649c925925961cd3fa12a4ac1
kynu <kynutamy@gmail.com>
2018-02-13 19:55:04 +0100

    AÃ±adir algunos cambios y la funcion x^4

--- Page/js/script.js
+++ Page/js/script.js
@@ -1,7 +1,21 @@
 /**
+ * Add event listener when page is loaded
+ */
+window.onload = function(){
+    document.getElementById("bt-cube").addEventListener("click",function(event){cube();})
+    document.getElementById("bt-cuarta").addEventListener("click",function(event){cuarta();})
+}
+/**
  * 
  */
-function cube() {
-var num = document.getElementById("n1");
-num.value = Math.pow(num.value, 3);
+function cube(){
+    let input = document.getElementById("input");
+    input.value = Math.pow(input.value, 3);
+}
+/**
+ * 
+ */
+function cuarta(){
+    let input = document.getElementById("input");
+    input.value = Math.pow(input.value, 4);
 }
