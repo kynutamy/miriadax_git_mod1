0f43571a3e6ab4e4382f39b706814bd96fa9b9d4
kynu <kynutamy@gmail.com>
2018-02-13 20:25:58 +0100

    AÃ±adir boton con la funcion sin(x)

--- Page/js/script.js
+++ Page/js/script.js
@@ -5,3 +5,7 @@ function cube() {
 var num = document.getElementById("n1");
 num.value = Math.pow(num.value, 3);
 }
+function sin(){
+var num = document.getElementById("n1");
+num.value = Math.sin(num.value);
+}
