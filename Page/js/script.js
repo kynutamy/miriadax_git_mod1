38bbbed562c1e116f5ab034513fe03bca0047f2b
kynu <kynutamy@gmail.com>
2018-02-13 20:41:13 +0100

    Se corrige el codigo resultante de juntar sin y master

--- Page/js/script.js
+++ Page/js/script.js
@@ -4,6 +4,7 @@
 window.onload = function(){
     document.getElementById("bt-cube").addEventListener("click",function(event){cube();})
     document.getElementById("bt-cuarta").addEventListener("click",function(event){cuarta();})
+    document.getElementById("bt-sin").addEventListener("click",function(event){sin();})
 }
 /**
  * Realiza la operación x^3 con el valor del input
@@ -19,7 +20,10 @@ function cuarta(){
     let input = document.getElementById("input");
     input.value = Math.pow(input.value, 4);
 }
+/**
+ * Realiza la operación sin(x) con el valor de input
+ */
 function sin(){
-var num = document.getElementById("n1");
-num.value = Math.sin(num.value);
+    let input = document.getElementById("input");
+    input.value = Math.sin(input.value);
 }
