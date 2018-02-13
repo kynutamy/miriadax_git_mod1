9f754889cda9afb507cc17fbaf0ff6ff5256605b
kynu <kynutamy@gmail.com>
2018-02-13 20:21:28 +0100

    se comenta el codigo

--- Page/js/script.js
+++ Page/js/script.js
@@ -1,19 +1,19 @@
 /**
- * Add event listener when page is loaded
+ * Añade los eventos al cargar la pagina
  */
 window.onload = function(){
     document.getElementById("bt-cube").addEventListener("click",function(event){cube();})
     document.getElementById("bt-cuarta").addEventListener("click",function(event){cuarta();})
 }
 /**
- * 
+ * Realiza la operación x^3 con el valor del input
  */
 function cube(){
     let input = document.getElementById("input");
     input.value = Math.pow(input.value, 3);
 }
 /**
- * 
+ * Realiza la operación x^4 con el valor de input
  */
 function cuarta(){
     let input = document.getElementById("input");
